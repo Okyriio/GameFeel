2ec093d8a924037655b7da9e7b0bca3cb9ada068
Okyriio <71374090+Okyriio@users.noreply.github.com>
2023-03-09 19:30:10 +0100

    player and enemy added

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+    public class AudioPlayer : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        [SerializeField] private List<AudioClip> _audioClips;
+        public AudioSource _audioSource;
+
+        private void Awake()
+        {
+            if (!_audioSource)
+                _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void Play()
+        {
+            int r = Random.Range(0, _audioClips.Count);
+            AudioClip clip = _audioClips[r];
+            _audioSource.clip = clip;
+            _audioSource.Play();
+        }
+    }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public delegate void HurtEvent(Transform attackerTransform);
+
+    public delegate void HealEvent();
+
+    public delegate void DeathEvent();
+
+    [SerializeField]
+    private int _maxHealthPoints = 10;
+
+    [SerializeField]
+    private int _healthPoints;
+    [SerializeField] private bool _destroyWhenKilled = true;
+
+    private Rigidbody2D _rb;
+
+    public HurtEvent OnHurt { get; set; }
+    public HealEvent OnHeal { get; set; }
+    public DeathEvent OnDeath { get; set; }
+    public bool CanReceiveDamage { get; set; } = true;
+    public bool IsDead => _healthPoints <= 0;
+    public int HealthPoints => _healthPoints;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _healthPoints = _maxHealthPoints;
+    }
+
+    public void HealPlayer(int heal)
+    {
+        _healthPoints += heal;
+        if (_healthPoints > _maxHealthPoints)
+        {
+            _healthPoints = _maxHealthPoints;
+        }
+
+        OnHeal?.Invoke();
+    }
+
+    public void Hurt(int hurtAmount, Transform attackerTransform = null)
+    {
+        if (!CanReceiveDamage) return;
+
+        _healthPoints -= hurtAmount;
+        OnHurt?.Invoke(attackerTransform);
+
+        if (!IsDead) return;
+
+        OnDeath?.Invoke();
+        if (!_destroyWhenKilled) return;
+        Destroy(gameObject, 0.2f);
+    }
+}
